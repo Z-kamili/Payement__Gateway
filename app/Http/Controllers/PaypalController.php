7e944140c1229f5f6be118db45d5df9c00961e94
Z-kamili <zakariakamili97@gmail.com>
2022-12-28 23:44:39 +0000

    more code

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use Srmklive\PayPal\Services\ExpressCheckout;
 
 use Illuminate\Http\Request;
 
 class PaypalController extends Controller
 {
+
+       public function payement()
+       {
+          $data = [];
+          $data['items'] = [
+           
+             [
+                'name' => 'Apple',
+                'price' => 100,
+                'description' => 'Macbook pro 14 inch',
+                'qte'=> 1,
+             ]
+
+          ];
+       }
     
 }
